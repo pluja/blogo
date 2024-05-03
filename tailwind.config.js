f45661a2e57b6d5a12cd5143edea3f6771dea140
pluja <github_pluja@r3d.red>
2024-05-03 16:58:58 +0200

    only monitor templates folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./templates/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
